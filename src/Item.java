c539b61cef0b0610e1d0040a89393b5cd0a219d1
Warren <warrenb95@live.co.uk>
2018-02-05 23:47:01 +0000

    Changed Item constructor
    Also changed the HashMap attribute to be more regid be setting data types

--- src/Item.java
+++ src/Item.java
@@ -1,11 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Item {
 	private String name;
 	private int goldValue;
-	private String attribute;
+	private Map<String, Integer> attribute = new HashMap<String, Integer>();
 	
 	Item(String itemName, int itmeGoldValue){
 		name = itemName;
 		goldValue = itmeGoldValue;
+	}
+	
+	Item(String itemName, int itmeGoldValue, Map itemAttribute){
+		name = itemName;
+		goldValue = itmeGoldValue;
+		attribute = itemAttribute;
+	}
+	
+	public Map getAttribute() {
+		return attribute;
 	}
 }
