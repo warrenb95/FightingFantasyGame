eb2c33bb4a841e2cede62fd24391e1053750e554
Warren <warrenb95@live.co.uk>
2018-02-05 20:54:04 +0000

    Item class
    Strarting to think about how i can implement the items class, need to figure out how i make item attributes affect player stats when the item is equiped or in the backpack.

---
+++ src/Item.java
@@ -0,0 +1,11 @@
+
+public class Item {
+	private String name;
+	private int goldValue;
+	private String attribute;
+	
+	Item(String itemName, int itmeGoldValue){
+		name = itemName;
+		goldValue = itmeGoldValue;
+	}
+}
